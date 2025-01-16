773284e18b620d0fcc35f3deb84c55405b9fc4c4
Yash Yadav <yashyadav88690@gmail.com>
2025-01-16 19:58:04 +0530

    final changes to handle pm and am timings

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 function updateClock(){
     let now= new Date();
-    let hours=now.getHours().toString().padStart(2,0);
+    let hours=now.getHours();
+    const meridiem=hours>12?'PM':'AM';
+    hours=hours%12||12;
     let minutes=now.getMinutes().toString().padStart(2,0);
     let seconds=now.getSeconds().toString().padStart(2,0);
-    let timeString = `${hours}:${minutes}:${seconds}`;
+    let timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
     document.getElementById("clock").textContent = timeString;
 }
 updateClock();
